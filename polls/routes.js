4ff329e48e162ea613fd99b69b47d0dd25254b4a
aikkyy <carolina.pestana.aguiar@gmail.com>
2024-02-26 21:10:54 +0000

    updated .env and .gitignore + solved task 1

--- polls/routes.js
+++ polls/routes.js
@@ -23,4 +23,23 @@ router.delete("/:id", async (req, res) => {
   res.status(200).json({ message: "poll deleted successfully" });
 });
 
+// GET - endpoint to get poll details by poll id
+router.get("/:id", async (req, res) => {
+  const pollId = req.params.id; //extract poll id from the url parameter
+
+  try {
+    const poll = await services.getPollById(pollId);
+
+    if (!poll) {
+      return res.status(404).json({ error: "Poll not found" }); //if poll not found, return error
+    }
+
+    res.status(200).json(poll); //if poll found, return it as a json response
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching poll details" });
+  }
+});
+
 module.exports = router;
