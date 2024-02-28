e5bcc9a79eff6e0f1677be395fb40fa9d659b47f
aikkyy <carolina.pestana.aguiar@gmail.com>
2024-02-28 20:35:28 +0000

    solved task 3

--- polls/services.js
+++ polls/services.js
@@ -38,16 +38,49 @@ async function deletePollById(pollId) {
 // function to create a poll
 async function createPoll(pollData) {
   try {
+    // transform each option string into an object with content and votes fields
+    const optionsWithVotes = pollData.options.map((option) => ({
+      content: option,
+      votes: 0,
+    }));
+
+    // replace original options array with transformed one
+    pollData.options = optionsWithVotes;
+
     const result = await db
       .getDB()
       .collection(db.pollsCollection)
       .insertOne(pollData);
 
-    // return new poll
+    // return the new poll
     return { _id: result.insertedId, ...pollData };
   } catch (error) {
     console.log(error);
     throw new Error("Failed to create poll");
+  }
+}
+
+// function to vote in option
+async function voteInOption(id, option) {
+  try {
+    const updateResult = await db
+      .getDB()
+      .collection(db.pollsCollection)
+      .updateOne(
+        { _id: db.toMongoID(id), "options.content": option },
+        { $inc: { "options.$.votes": 1 } }
+      );
+
+    if (updateResult.matchedCount === 0) {
+      throw new Error("Poll or option not found");
+    }
+    if (updateResult.modifiedCount === 0) {
+      throw new Error("Failed to cast vote, option may not exist");
+    }
+    return { message: "Vote successfully cast" };
+  } catch (error) {
+    console.error("Failed to vote in option:", error);
+    throw new Error("Failed to vote in option: " + error.message);
   }
 }
 
@@ -56,4 +89,5 @@ module.exports = {
   getAllPolls,
   deletePollById,
   createPoll,
+  voteInOption,
 };
