3b1285b4d6ac7f57b4911b0c5f5fae2453bfb6cb
aikkyy <carolina.pestana.aguiar@gmail.com>
2024-02-28 22:48:34 +0000

    solved task 7

--- auth/routes.js
+++ auth/routes.js
@@ -16,12 +16,33 @@ router.post("/signin", async (req, res) => {
     return res.status(401).json({ error: "unauthorized" });
   }
 
+  // check if user is blocked
+  if (user.isLocked) {
+    return res.status(403).json({ error: "account locked" });
+  }
+
   const isValidPwd = await services.validatePassword(
     value.password,
     user.password
   );
   if (!isValidPwd) {
-    return res.status(401).json({ error: "unauthorized" });
+    user.loginAttempts = (user.loginAttempts || 0) + 1;
+    if (user.loginAttempts > 3) {
+      user.isLocked = true; //block user
+    }
+
+    // update user data in database
+    await services.updateUser({
+      _id: user._id,
+      loginAttempts: user.loginAttempts,
+      isLocked: user.isLocked,
+    });
+
+    if (user.isLocked) {
+      return res.status(403).json({ error: "account locked" });
+    } else {
+      return res.status(401).json({ error: "unauthorized" });
+    }
   }
 
   const token = services.generateAccessToken(user._id);
@@ -29,16 +50,21 @@ router.post("/signin", async (req, res) => {
   res.status(200).json({ result: "ok", token });
 });
 
+// updated endpoint
 router.post("/signup", async (req, res) => {
-  const { error, value } = schemas.signupSchema.validate(req.body);
+  const newUserData = req.body;
+
+  // validate request body against schema
+  const { error, value } = schemas.signupSchema.validate(newUserData);
   if (error) {
     return res
       .status(400)
       .json({ error: "invalid body", details: error.details });
   }
+  const { name, email, password } = value;
 
-  const user = await services.findUserByEmail(value.email);
-  if (user) {
+  const userEmail = await services.findUserByEmail(value.email);
+  if (userEmail) {
     return res.status(400).json({ error: "email already in use" });
   }
 
